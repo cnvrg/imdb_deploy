83f3116b20e74b5239fbc63bd788064761265d8a
Leah <leah@cnvrg.io>
2019-10-15 21:37:28 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -20,7 +20,7 @@ word_to_id["<PAD>"] = 0
 word_to_id["<START>"] = 1
 word_to_id["<UNK>"] = 2
 
-model = keras.models.load_model("output/sentiment2.model.h5")
+model = keras.models.load_model("sentiment2.model.h5")
 model._make_predict_function()
 
 reverse_word_index = dict([(value, key) for (key, value) in word_to_id.items()])
